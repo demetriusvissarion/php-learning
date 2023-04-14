c9a8c4986874cb6d585455049e6a7aa8c87f4182
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-14 16:13:49 +0100

    composer autoloader

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,11 +5,6 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
-require 'core/Router.php';
-require 'core/Request.php';
-
 $app ['database'] = new QueryBuilder(
 	Connection::make($app['config']['database'])
 );
